fe8d960af1a9d7a6d90544793686a203c17caed7
jdwitten <jdwitten11@gmail.com>
2016-12-12 22:27:02 -0500

    fixed port

--- test.js
+++ test.js
@@ -33,6 +33,6 @@ app.get('/', function (req, res) {
 	});
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT, function () {
+  console.log('App running')
 })
