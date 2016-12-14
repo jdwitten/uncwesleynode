08c40c031c061a7fe6b06392dbf1d8a97c93613f
jdwitten <jdwitten11@gmail.com>
2016-12-13 23:41:13 -0500

    added mysql

--- test.js
+++ test.js
@@ -2,25 +2,22 @@ var express = require('express');
 var app = express();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+
+
+app.get('/', function (req, res) {
+  var connection = mysql.createConnection({
     host: "us-cdbr-azure-east-c.cloudapp.net",
     user : "bddfe4567fcee0",
     password : "aaa47743",
     database: "uncwesley"
-})
-
-connection.connect(function(err){
+  })
+  connection.connect(function(err){
   if(!err) {
     console.log("Database is connected ... nn");    
   }else {
     console.log("Error connecting database ... nn");    
-}
-
-
-});
-
-app.get('/', function (req, res) {
-connection.query('SELECT * from Events', function(err, rows, fields) {
+  }
+  connection.query('SELECT * from events', function(err, rows, fields) {
   connection.end();
   if (!err)
     console.log('The solution is: ', rows);
